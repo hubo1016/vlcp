6407d8c86c97e3a1b1d3adf252dd1ae01525cb8e
hubo1016 <hubo1016@126.com>
2015-11-17 20:13:01 +0800

    setup scripts

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+'''
+Created on 2015/11/17
+
+@author: hubo
+'''
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+
+VERSION = '0.9.0'
+
+setup(name='vlcp',
+      version=VERSION,
+      description='Full stack framework for SDN Controller, support Openflow 1.0, Openflow 1.3, and Nicira extensions.',
+      author='Hu Bo',
+      author_email='hubo1016@126.com',
+      license="http://www.apache.org/licenses/LICENSE-2.0",
+      url='http://github.com/hubo1016/vlcp',
+      keywords=['SDN', 'VLCP', 'Openflow'],
+      test_suite = 'tests',
+      use_2to3=False,
+      packages=find_packages(include = ('vlcp*',)))
