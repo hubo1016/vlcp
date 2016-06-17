c3a7a7d9370d4e3e10db200dcee1a1a7d8b624b1
hubo1016 <hubo1016@126.com>
2016-06-17 20:51:21 +0800

    bug fix

--- vlcp/utils/flowupdater.py
+++ vlcp/utils/flowupdater.py
@@ -106,7 +106,7 @@ class FlowUpdater(RoutineContainer):
                 if self._updatedset:
                     if any(v.getkey() in _initialkeys for v in self._updatedset):
                         continue
-                lastkeys = self._savedkeys
+                lastkeys = set(self._savedkeys)
                 self._savedkeys, self._savedresult = self.retvalue
                 removekeys = tuple(lastkeys.difference(self._savedkeys))
                 if self._dataupdateroutine:
