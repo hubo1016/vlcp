d24b6cc255a0fcae884028cae2f23467f6400974
hubo1016 <hubo1016@126.com>
2016-10-19 23:23:29 +0800

    bug fix: parameter not match

--- vlcp/service/kvdb/redisnotifier.py
+++ vlcp/service/kvdb/redisnotifier.py
@@ -50,8 +50,8 @@ def _str(s):
 
 class _Notifier(RoutineContainer):
     _logger = logging.getLogger(__name__ + '.Notifier')
-    def __init__(self, vhostbind, prefix, scheduler=None, daemon=False, singlecastlimit = 256, deflate = False):
-        RoutineContainer.__init__(self, scheduler=scheduler, daemon=daemon)
+    def __init__(self, vhostbind, prefix, scheduler=None, singlecastlimit = 256, deflate = False):
+        RoutineContainer.__init__(self, scheduler=scheduler, daemon=False)
         self.vhostbind = vhostbind
         self.prefix = _bytes(prefix)
         self._matchers = {}
