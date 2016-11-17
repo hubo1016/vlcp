24d7d2cb5724417f794faf4200e850816c20e19f
gaopl <gaopl@jiedaibao.com>
2016-11-17 17:17:34 +0800

    将plugin中删除结构时依赖性的检查调整到上层，优化结构！

--- vlcp/service/sdn/plugins/networklocaldriver.py
+++ vlcp/service/sdn/plugins/networklocaldriver.py
@@ -468,12 +468,6 @@ class NetworkLocalDriver(Module):
                 if not phymap:
                     raise ValueError("physicalnetwork map key object not existed " + network["phynetid"]) 
 
-                if len(lgnetmap.ports.dataset()):
-                    raise ValueError("there ports on logicnet remove it before")
-
-                if lgnetmap.subnets.dataset():
-                    raise ValueError("there subnets on logicalnet , remove it before")
-
                 values[0].set.dataset().discard(lgnet.create_weakreference())
                 #del phymap.network_allocation[str(lgnet.sid)]
                 phymap.logicnetworks.dataset().discard(lgnet.create_weakreference())
