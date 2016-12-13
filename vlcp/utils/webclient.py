eba27ffda326364706397c50ed62ae15c3bc20cd
hubo1016 <hubo1016@126.com>
2016-12-13 16:39:49 +0800

    - show JSON format error information by default - auto decompress error information

--- vlcp/utils/webclient.py
+++ vlcp/utils/webclient.py
@@ -429,6 +429,12 @@ class WebClient(Configurable):
                 if resp.iserror and not ignorewebexception:
                     try:
                         exc = WebException(resp.fullstatus)
+                        if autodecompress and resp.stream:
+                            ce = resp.get_header('Content-Encoding', '')
+                            if ce.lower() == 'gzip' or ce.lower() == 'x-gzip':
+                                resp.stream.getEncoderList().append(encoders.gzip_decoder())
+                            elif ce.lower() == 'deflate':
+                                resp.stream.getEncoderList().append(encoders.deflate_decoder())
                         for m in resp.stream.read(container, 4096):
                             yield m
                         exc.response = resp
