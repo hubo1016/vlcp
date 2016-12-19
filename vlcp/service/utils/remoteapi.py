b9b7b33178b825ae6ae023ccc5f7a2522cd2587b
gaopl <gaopl@jiedaibao.com>
2016-12-19 15:14:49 +0800

    完善 remoteapi 功能模块！

--- vlcp/service/utils/remoteapi.py
+++ vlcp/service/utils/remoteapi.py
@@ -1,8 +1,10 @@
+import json
+
 from vlcp.config import defaultconfig
 
 from vlcp.event import RoutineContainer
 
-from vlcp.utils.webclient import WebClient
+from vlcp.utils.webclient import WebClient, WebException
 
 from vlcp.server.module import Module,callAPI, api
 
@@ -18,19 +20,36 @@ class RemoteCall(Module):
         self.wc = WebClient()
         self.createAPI(api(self.call,self.app_routine))
 
-    def call(self,remote_module,method,**kwargs):
+    def call(self,remote_module,method,timeout,params):
         self._logger.info("remote call remote_module %r",remote_module)
         self._logger.info("remote call method %r", method)
-        self._logger.info("remote call kwargs %r", kwargs)
+        self._logger.info("remote call kwargs %r", params)
 
-        if None:
-            yield
+        if remote_module and remote_module in self.target_url_map:
+            endpoints = self.target_url_map[remote_module]
+            params = json.dumps(params).encode("utf-8")
+            for endpoint in endpoints:
+                url = endpoint + "/" + remote_module + "/" + method
+
+                try:
+                    for m in self.wc.urlgetcontent(self.app_routine,url,params,b'POST',
+                                                   {"Content-Type":"application/json"},timeout=timeout):
+                        yield m
+                except WebException as e:
+                    # this endpoint connection error , try next url
+                    self._logger.warning(" url (%r) post error %r , break ..",url,e)
+                    break
+                except Exception:
+                    self._logger.warning(" url (%r) connection error , try next ..", url)
+                    continue
+
+        else:
+            self._logger.warning(" target (%r) url not existed, ignore it ",remote_module)
 
         self.app_routine.retvalue = []
 
 
-def remoteAPI(container,targetname,name,params={}):
-    args = {"remote_module":targetname,"method":name}
-    args.update(params)
-    for m in callAPI(container,"remotecall","call",params=args,timeout=20):
+def remoteAPI(container,targetname,name,params={},timeout=60.0):
+    args = {"remote_module":targetname,"method":name,"timeout":timeout,"params":params}
+    for m in callAPI(container,"remotecall","call",params=args,timeout=timeout + 20):
         yield m
