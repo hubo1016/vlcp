705b3cb617636fe2fe1bc427ec44eb97ce98d40b
hubo1016 <hubo1016@126.com>
2017-01-14 12:41:36 +0800

    RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -119,9 +119,19 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-html_style = 'css/customized.css'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    html_theme = 'sphinx_rtd_theme'
+    html_style = 'css/customized.css'
+else:
+    html_context = { 
+        'css_files': [
+            'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+            'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+            '_static/css/customized.css',
+        ],  
+    }   
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
