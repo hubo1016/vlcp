71e8e2eb5b5f60d89be7dff29bc06ee2206a29e1
hubo1016 <hubo1016@126.com>
2017-02-06 19:52:52 +0800

    install OpenvSwitch

--- ci_test.sh
+++ ci_test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -xe
 sudo python -m unittest discover
 if [ "${TRAVIS_EVENT_TYPE}" == "cron" -o "${TRAVIS_EVENT_TYPE}" == "pull_request" ]; then
+	sudo apt-get install -y openvswitch-switch
 	python setup.py bdist_wheel
 	wget https://github.com/hubo1016/vlcp-controller-test/archive/master.tar.gz -O ./vlcp-controller-test.tar.gz
 	tar -xzvf vlcp-controller-test.tar.gz
 	cp dist/*.whl vlcp-controller-test-master/
 	pushd vlcp-controller-test-master/
-	bash -xe starttest.sh
+	sudo bash -xe starttest.sh
 fi
