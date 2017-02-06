acc091da15440cbbef31bbaa90d0d12bd6c22499
hubo1016 <hubo1016@126.com>
2017-02-06 19:40:01 +0800

    integration test for pull requests

--- ci_test.sh
+++ ci_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 sudo python -m unittest discover
-if [ "${TRAVIS_EVENT_TYPE}" == "cron" ]; then
+if [ "${TRAVIS_EVENT_TYPE}" == "cron" -o "${TRAVIS_EVENT_TYPE}" == "pull_request" ]; then
 	python setup.py bdist_wheel
 	wget https://github.com/hubo1016/vlcp-controller-test/archive/master.tar.gz -O ./vlcp-controller-test.tar.gz
 	tar -xzvf vlcp-controller-test.tar.gz
