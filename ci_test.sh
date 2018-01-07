c60d4f9d1c74c26cb8d70fd5645df51f6c33716a
gaopl <gaopl@jiedaibao.com>
2018-01-07 15:46:47 +0800

    add codecov report coverage

--- ci_test.sh
+++ ci_test.sh
@@ -11,12 +11,14 @@ fi
 [ -f "/home/travis/virtualenv/${venv}/bin/activate" ] && source /home/travis/virtualenv/${venv}/bin/activate
 
 python --version
-python -m unittest discover
+coverage --version
+coverage run -m unittest discover
+
 if [ "${TRAVIS_EVENT_TYPE}" == "cron" -o "${TRAVIS_EVENT_TYPE}" == "pull_request" -o "${TRAVIS_TAG:-}" != "" ]; then
 	python setup.py bdist_wheel
 	wget https://github.com/hubo1016/vlcp-controller-test/archive/master.tar.gz -O ./vlcp-controller-test.tar.gz
 	tar -xzvf vlcp-controller-test.tar.gz
 	cp dist/*.whl vlcp-controller-test-master/
 	pushd vlcp-controller-test-master/
-	bash -xe starttest.sh $venv ${KV_DB}
+	bash -xe starttest.sh $venv ${KV_DB} "coverage"
 fi
